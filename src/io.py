4ff295c14ad18a6b9b25729fc6cc85cea37dc239
supplyandcommand <42962106+supplyandcommand@users.noreply.github.com>
2023-11-08 10:35:32 +0100

    scipdf parser

---
+++ src/io.py
@@ -0,0 +1,39 @@
+"""
+"""
+
+import os
+import json
+import scipdf
+from tqdm import tqdm
+
+
+def get_file_paths(folder_path: str) -> tuple[list, list]:
+    """
+    """
+
+    pdf_paths = []
+    other_paths = []
+    for root, directories, files in os.walk(folder_path):
+        for filename in files:
+            filepath = os.path.join(root, filename)
+            if filename.endswith('.pdf'):
+                pdf_paths.append(filepath)
+            else:
+                other_paths.append(filepath)
+
+    return pdf_paths, other_paths
+
+
+if __name__ == "__main__":
+
+    ROOTPATH = "data/UTA publications/"
+    OUTPATH = "data/UTA publications/UTA_pubs.json"
+    pdf_paths, other_paths = get_file_paths(ROOTPATH)
+
+    with open(OUTPATH, "w") as file:
+        for path in tqdm(pdf_paths):
+            parsed = scipdf.parse_pdf_to_dict(path)
+            # Use the json.dumps method to serialize the dictionary to a JSON string
+            json_string = json.dumps(parsed)
+            # Write the JSON string with a newline character to separate objects
+            file.write(json_string + "\n")
