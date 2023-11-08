2e87d9c50317f17ef45162fb024c03f7018d080c
supplyandcommand <42962106+supplyandcommand@users.noreply.github.com>
2023-11-08 10:35:00 +0100

    fix: -y

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ python -m spacy download en_core_web_sm
 
 # java for runing grobit
 sudo apt update
-sudo apt install openjdk-11-jre-headless -Y
+sudo apt install openjdk-11-jre-headless -y
 java -version
 
 # grobit backend
