1b34b69bdba1d56d1297761d1fd17212cf9176ce
supplyandcommand <42962106+supplyandcommand@users.noreply.github.com>
2023-11-16 16:06:09 +0100

    comment out sentence-transformers

--- src/features/jina_emb.py
+++ src/features/jina_emb.py
@@ -1,10 +1,10 @@
-"""Placeholder for general Jina embeddings functions.
-"""
+# """Placeholder for general Jina embeddings functions.
+# """
 
-import os
-import datasets
-from sentence_transformers import SentenceTransformer
+# import os
+# import datasets
+# from sentence_transformers import SentenceTransformer
 
 
-def load_mddel() -> SentenceTransformer:
-    return SentenceTransformer("jinaai/jina-embeddings-v2-base-en")
+# def load_mddel() -> SentenceTransformer:
+#     return SentenceTransformer("jinaai/jina-embeddings-v2-base-en")
